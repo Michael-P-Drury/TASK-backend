42fdcb63ea021620d15a4234a7e52920695dfef9
Michael Drury <120413504+Michael-P-Drury@users.noreply.github.com>
2026-03-02 15:49:27 +0000

    Refactor S3 functionality with type hints and docstrings
    Refactor S3 functionality to include type hints and improve error handling. Added docstrings for better code documentation.

--- app/data_storage/s3_functionality.py
+++ app/data_storage/s3_functionality.py
@@ -1,3 +1,7 @@
+'''
+holds functionality for s3 file storage
+'''
+
 import aioboto3
 from dotenv import load_dotenv
 import os
@@ -6,7 +10,14 @@ from botocore.config import Config
 
 load_dotenv()
 
-async def upload_user_output_file(username, file_name, file_content):
+async def upload_user_output_file(username: str, file_name: str, file_content):
+    '''
+    inputs:
+    username: str - users username
+    file_name: str - filename for output file
+    file_content - file content to be saved (can be different formats)
+    '''
+    
     try:
 
         s3_url = os.getenv('S3_URL')
@@ -38,18 +49,28 @@ async def upload_user_output_file(username, file_name, file_content):
         return 400, 'Failed to upload user image'
 
 
-async def download_user_output_file(username, file_name):
+async def download_user_output_file(username: str, file_name: str):
+    '''
+    inputs:
+    username: str - users username
+    file_name: str - file name to be downloaded
+
+    for downloading a users file
+    '''
+
     try:
 
+        # get env variables
         s3_url = os.getenv('S3_URL')
-
         aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
         aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
+        # creates entire file path for file to be downloaded
         file_path = f'user_files/{username}/output_files/{file_name}'
 
         session = aioboto3.Session()
 
+        # crates async session with the s3 bucket, and reads the body for the file to download, returning the content
         async with session.client(
             's3',
             endpoint_url=s3_url,
@@ -64,24 +85,29 @@ async def download_user_output_file(username, file_name):
             return 200, f'File {file_name} downloaded', file_content
     
     except Exception as e:
-        print(e)
         return 400, 'Failed to upload user image', None
     
 
 
-async def upload_user_working_file(username, file_name, file_content):
+async def upload_user_working_file(username: str, file_name: str, file_content):
+    '''
+    inputs:
+    username: str - users username
+    file_name: str - filename for output file
+    file_content - file content to be saved (can be different formats)
+    '''
 
     try:
 
+        #l oads env variables
         s3_url = os.getenv('S3_URL')
-
         aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
         aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
         file_path = f'user_files/{username}/working_files/{file_name}'
 
         session = aioboto3.Session()
-
+        # creates async sioboto3 session witrh s3 bucket and uploads new file
         async with session.resource(
             's3',
             endpoint_url=s3_url,
@@ -100,12 +126,19 @@ async def upload_user_working_file(username, file_name, file_content):
 
 
 
-async def delete_user_working_file(username, file_name):
+async def delete_user_working_file(username: str, file_name: str):
+    '''
+    inputs:
+    username: str - users username
+    file_name: str - file name to be deleted
+    '''
 
     try:
+        # creates entire path for file to be deleted
         file_path = f'user_files/{username}/working_files/{file_name}'
-        session = aioboto3.Session()
 
+        # creates async session and then deletes the file
+        session = aioboto3.Session()
         async with session.client(
             's3',
             endpoint_url=os.getenv('S3_URL'),
@@ -121,12 +154,19 @@ async def delete_user_working_file(username, file_name):
         return 400, 'Failed to upload user image'
     
 
-async def delete_user_output_file(username, file_name):
+async def delete_user_output_file(username: str, file_name: str):
+    '''
+    inputs:
+    username: str - users username
+    file_name: str - file name to be deleted
+    '''
 
     try:
+        # creates file path for file
         file_path = f'user_files/{username}/output_files/{file_name}'
-        session = aioboto3.Session()
 
+        # creates async sessiobn with s3 bucket and deletes file
+        session = aioboto3.Session()
         async with session.client(
             's3',
             endpoint_url=os.getenv('S3_URL'),
@@ -142,11 +182,19 @@ async def delete_user_output_file(username, file_name):
         return 400, 'Failed to upload user image'
 
 
-async def get_user_working_files(username):
+async def get_user_working_files(username: str):
+    '''
+    inputs:
+    username: str - users username
+
+    gets the users working files
+    '''
     try:
+        # creates path for user's working files
         working_files_path = f'user_files/{username}/working_files/'
-        session = aioboto3.Session()
 
+        # creates async session with s3 bucket and returns list of users working files
+        session = aioboto3.Session()
         async with session.client(
             's3',
             endpoint_url=os.getenv('S3_URL'),
@@ -168,17 +216,24 @@ async def get_user_working_files(username):
 
 
 
-async def get_user_output_files(username):
+async def get_user_output_files(username: str):
+    '''
+    inputs:
+    username: str - users username
+    '''
     try:
+        # gets path for user's output files
         working_files_path = f'user_files/{username}/output_files/'
-        session = aioboto3.Session()
 
+        # create async session with s3 client and returns list of user's output files
+        session = aioboto3.Session()
         async with session.client(
             's3',
             endpoint_url=os.getenv('S3_URL'),
             aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
             aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
         ) as s3:
+
             full_files_list = await s3.list_objects_v2(Bucket='task-user-storage', Prefix=working_files_path)
 
             return_files = []
@@ -191,4 +246,34 @@ async def get_user_output_files(username):
 
     except:
         return 400, 'Error getting user files', []
+
+
+
+async def delete_user_s3_data(username: str):
+    '''
+    deletes all objects for user from s3
+    '''
+
+    folder_path = f'user_files/{username}/'
+    
+    try:
+        session = aioboto3.Session()
+        async with session.client(
+            's3',
+            endpoint_url=os.getenv('S3_URL'),
+            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+        ) as s3:
+            
+            full_files_list = await s3.list_objects_v2(Bucket='task-user-storage', Prefix=folder_path)
+
+            files_list = full_files_list.get('Contents', [])
+
+            for file_path in files_list:
+                await s3.delete_object(Bucket='task-user-storage', Key=file_path['Key'])
+
+            return 200, f'user files deleted successfully'
+
+    except Exception as e:
+        return 400, f'Error deleting user files'
 
