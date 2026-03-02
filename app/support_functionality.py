97dd3b1d57bf2cf3a73155f31219875e3ccf24c2
Michael Drury <120413504+Michael-P-Drury@users.noreply.github.com>
2026-03-02 15:40:04 +0000

    Enhance support functionality for modularity
    Refactor support functionality for modularity in application for controller agent.

--- app/support_functionality.py
+++ app/support_functionality.py
@@ -1,3 +1,7 @@
+'''
+support functionality for modularity in application for controller agent
+'''
+
 from .tools.tools_directory import tools_dict
 from .user.user_account import get_user_chat_history, set_chat_history
 import time
@@ -7,8 +11,21 @@ import pypandoc
 import tempfile
 import os
 
-async def seperate_tools(decision_text):
+async def seperate_tools(decision_text: str):
+    '''
+    inputs:
+
+    decision_text: str - formats text response from genai of tyool decision into organised dictionary
+
+    outputs:
     
+    dict - seperated out dictionary for different types of tools for processing.
+
+
+    seperates out genai text into organised dictionary for processing
+    '''
+    
+    # initiates out different types of tools as empty strings/ lists
     main_tool = ''
     support_tools = []
     quality_tools = []
@@ -16,6 +33,8 @@ async def seperate_tools(decision_text):
     extra_main_tools = []
     seperated_tools_text_list = []
     
+    # loops through each line in response (each tool decision is split by a new line) and gets tool type/ lack of presence from tools dictionary
+    # adding it to string/ list for that tool
     for tool in decision_text.split('\n'):
         tool_id = tool.strip()
 
@@ -38,6 +57,7 @@ async def seperate_tools(decision_text):
         else:
             halucinated_tools.append(tool_id)
 
+    # created out a formatted text list to be turned intop a string (to display to users adds readability).
     if main_tool:
         seperated_tools_text_list.append(f'Main tool: *{main_tool}*')
 
@@ -60,27 +80,55 @@ async def seperate_tools(decision_text):
 
 
 
-async def add_chat_history(username, chat_sender, chat_message, full_history):
+async def add_chat_history(username: str, chat_sender: str, chat_message: str, full_history: bool):
+    '''
+    inputs:
+    username: str - users username
+    chat_sender: str - user or TASK (sender of the message to be added to chat)
+    chat_message: str - the message to be saved
+    full_history: bool - True if it is to be added to the full history and False if it is to be added to the condensed history
 
+    outputs:
+    chat_history: str - returns chat history to be used
+
+    adds to chat history (condensed and full based on full_history bool)
+    '''
+
+    # gets the current history which is to be added to
     current_chat_history = await get_user_chat_history(username, full_history)
-
     current_chat_history = current_chat_history['chat_history']
 
+    # there there is already a history, add to it and if not, initiate chat history, formatted in markdown
     if current_chat_history:
         new_chat_history = current_chat_history + '\n\n**' + chat_sender + ':**\n' + chat_message
 
     else:
         new_chat_history = '**' + chat_sender + ':**\n' + chat_message
 
+    # sets the chat history
     await set_chat_history(username, new_chat_history, full_history)
 
+    # returns the chat history
     return new_chat_history
 
 
 
-async def run_support_tools(username, support_tools_list):
+async def run_support_tools(username: str, support_tools_list: list):
+    '''
+    inputs:
+    username: str - users username
+    support_tools_list: list[str] list of support tool ids to run
+
+    outputs:
+    list[str] - responses from all support tools
+
+    runs support tools
+    '''
+
+    # initiaqlises async tasks to run
     async_tasks = []
 
+    # loops through each support tool adding run function to async tasks to run
     for tool in support_tools_list:
 
         support_func = await get_run_tool_function(tool)
@@ -89,17 +137,22 @@ async def run_support_tools(username, support_tools_list):
 
         async_tasks.append(support_func(username, support_info))
 
+    # adds to full chat history that suport tools are running
     full_chat_history_text = f'Running support tools: {" ,".join(support_tools_list)}'
 
-    support_tools_start = time.time()
-
     await add_chat_history(username, 'TASK', full_chat_history_text, True)
+
+    # measures time to run and runs tasks
+
+    support_tools_start = time.time()
 
     support_tools_responses = await asyncio.gather(*async_tasks)
 
     support_tools_end = time.time()
 
     support_tools_time = support_tools_end - support_tools_start
+
+    # adds to full chat history that the support tools have ran
 
     full_chat_history_text = f'Ran support tools: {" ,".join(support_tools_list)} in {support_tools_time}s'
 
@@ -109,12 +162,27 @@ async def run_support_tools(username, support_tools_list):
 
 
 
-async def run_main_tool(username, main_tool, support_tools_responses, relevant_chat_context):
+async def run_main_tool(username: str, main_tool: str, support_tools_responses: list, relevant_chat_context: str):
+    '''
+    inputs:
+    username: str - users username
+    main_tool: str - id for main tool
+    support_tool_responses: list[str] - list of responses from support tools
+    relevant_chatr_context: str - relevant chat context for running main tool
+
+    outputs:
+    main_tool_response: str - response from main tool
+
+    runs main tool adding to full chat history that it has ran with time to run
+    '''
+
+    # adds to full chat history that main tool is running
     full_chat_history_text = f'Running main tool: {main_tool}'
 
-    main_tool_start = time.time()
-
     await add_chat_history(username, 'TASK', full_chat_history_text, True)
+
+    # measure time to run and run main tool
+    main_tool_start = time.time()
 
     main_func = await get_run_tool_function(main_tool)
 
@@ -124,6 +192,7 @@ async def run_main_tool(username, main_tool, support_tools_responses, relevant_c
 
     main_tool_time = main_tool_end - main_tool_start
 
+    # add to full chat history that main tool has ran and with time to run then return response from main tool
     full_chat_history_text = f'Ran main tool: {main_tool} in {main_tool_time}s'
 
     await add_chat_history(username, 'TASK', full_chat_history_text, True)
@@ -140,18 +209,20 @@ async def create_docx_from_markdown(markdown_text):
     output:
     bytes object
 
-    use:
     converts input of markdown text into a word file bytes object.
     '''
 
+    # creates a temporary file (needed to context to .docx)
     temp_out_file =  tempfile.NamedTemporaryFile(suffix='.docx', delete=False)
     
     temp_output_path = temp_out_file.name
 
     temp_out_file.close()
-    
+
+    # use pypandoc to convert from markdown to docx file
     pypandoc.convert_text(markdown_text, 'docx', format='md', outputfile=temp_output_path)
     
+    # reads file, removes file and returns docx file bytes
     with open(temp_output_path, 'rb') as f:
         docx_bytes = f.read()
 
